70fe068146c44e67758014130d7df1030157708e
Michiel van Beers <michiel.van.beers@plat4mation.com>
2023-03-26 17:55:50 +0200

    Add ability to save output and better logging

--- plugin/main.py
+++ plugin/main.py
@@ -2,6 +2,8 @@
 
 import os
 import csv
+import logging
+from datetime import datetime
 from flox import Flox  # noqa: E402
 import webbrowser  # noqa: E402
 import requests  # noqa: E402
@@ -15,14 +17,82 @@ class ChatGPT(Flox):
         self.model = self.settings.get("model")
         self.prompt_stop = self.settings.get("prompt_stop")
         self.default_system_prompt = self.settings.get("default_prompt")
+        self.save_conversation_setting = self.settings.get("save_conversation")
+        self.log_level = self.settings.get("log_level")
+        self.logger_level(self.log_level)
 
         try:
             self.csv_file = open("system_messages.csv", encoding="utf-8", mode="r")
-            self.prompts = csv.DictReader(self.csv_file, delimiter=";")
+            reader = csv.DictReader(self.csv_file, delimiter=";")
+            self.prompts = list(reader)
+            [logging.debug(f"Found prompt: {row}") for row in self.prompts]
+
         except FileNotFoundError:
             self.prompts = None
+            logging.error("Unable to open system_messages.csv")
 
-    def send_prompt(self, prompt: str, system_message: str) -> str:
+    def query(self, query: str) -> None:
+        if not self.api_key:
+            self.add_item(
+                title="Unable to load the API key",
+                subtitle="""
+                Please make sure you've added 
+                a valid API key in the settings.
+                """,
+            )
+            return
+        if self.prompts is None:
+            self.add_item(
+                title="Unable to load the system prompts from CSV",
+                subtitle="""
+                Please validate that the plugins folder
+                contains a valid system_prompts.csv
+                """,
+                method=self.open_plugin_folder,
+            )
+            return
+        if query.endswith(self.prompt_stop):
+            prompt, prompt_keyword, system_message = self.split_prompt(query)
+
+            answer, prompt_timestamp, answer_timestamp = self.send_prompt(
+                prompt, system_message
+            )
+
+            filename = None
+            prompt = prompt + system_message
+            if self.save_conversation_setting:
+                filename = self.save_conversation(
+                    prompt_keyword, prompt, prompt_timestamp, answer, answer_timestamp
+                )
+
+            if answer:
+                answer = answer.lstrip("\n").lstrip("\n")
+                short_answer = self.ellipsis(answer, 30)
+
+                self.add_item(
+                    title="Copy to clipboard",
+                    subtitle=f"Answer: {short_answer}",
+                    method=self.copy_answer,
+                    parameters=[answer],
+                )
+
+                self.add_item(
+                    title="Open in text editor",
+                    subtitle=f"Answer: {short_answer}",
+                    method=self.open_in_editor,
+                    parameters=[filename, answer],
+                )
+
+        else:
+            self.add_item(
+                title=f"Type your prompt and end with {self.prompt_stop}",
+                subtitle=f"Current model: {self.model}",
+            )
+        return
+
+    def send_prompt(
+        self, prompt: str, system_message: str
+    ) -> tuple[str, datetime, datetime]:
         """
         Query the OpenAI end-point
         """
@@ -46,7 +116,11 @@ class ChatGPT(Flox):
 
         data = json.dumps(body)
 
+        prompt_timestamp = datetime.now()
+        logging.debug(f"Sending request with data: {data}")
         response = requests.request("POST", url, headers=headers, data=data)
+        logging.debug(f"Response: {response}")
+        answer_timestamp = datetime.now()
 
         result = ""
         response_json = response.json()
@@ -57,73 +131,77 @@ class ChatGPT(Flox):
             self.add_item(
                 title="An error occurred", subtitle=response_json["error"]["message"]
             )
-        return result
-
-
-    def query(self, query: str) -> None:
-        if not self.api_key:
-            self.add_item(
-                title="Unable to load the API key",
-                subtitle="""
-                Please make sure you've added 
-                a valid API key in the settings.
-                """,
+            logging.error(
+                f"API returned {response.status_code} with message: {response_json}"
             )
-            return
-        if self.prompts is None:
-            self.add_item(
-                title="Unable to load the system prompts from CSV",
-                subtitle="""
-                Please validate that the plugins folder
-                contains a valid system_prompts.csv
-                """,
-                method=self.open_plugin_folder,
-            )
-            return
-        if query.endswith(self.prompt_stop):
-            prompt = query.rstrip(self.prompt_stop)
-            prompt_key_word = prompt.split(" ")[0].lower()
+        return result, prompt_timestamp, answer_timestamp
 
-            system_message = ""
+    def save_conversation(
+        self,
+        keyword: str,
+        prompt: str,
+        prompt_timestamp: datetime,
+        answer: str,
+        answer_timestamp: datetime,
+    ) -> str:
+        filename = f"Conversations '{keyword}' keyword.txt"
+        formatted_prompt_timestamp = prompt_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        formatted_answer_timestamp = answer_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        new_content = f"""[{formatted_prompt_timestamp}] User: {prompt}\n[{formatted_answer_timestamp}] ChatGPT: {answer}\n"""  # noqa: E501
+
+        if os.path.exists(filename):
+            try:
+                with open(filename, "r") as file:
+                    existing_content = file.read()
+            except PermissionError:
+                logging.error(PermissionError)
+        else:
+            existing_content = ""
+
+        new_content = new_content + existing_content
+
+        try:
+            with open(filename, "w") as file:
+                file.write(new_content)
+        except PermissionError:
+            logging.error(PermissionError)
+
+        return filename
+
+    def split_prompt(self, query: str) -> tuple[str, str, str]:
+        prompt = query.rstrip(self.prompt_stop).strip()
+        prompt_array = prompt.split(" ")
+        prompt_keyword = prompt_array[0].lower()
+
+        system_message = ""
+
+        for row in self.prompts:
+            if row["Key Word"] == prompt_keyword:
+                system_message = row["System Message"]
+
+        if not system_message:
+            prompt_keyword = self.default_system_prompt
 
             for row in self.prompts:
-                if row["Key Word"] == prompt_key_word:
+                if row["Key Word"] == self.default_system_prompt:
                     system_message = row["System Message"]
-                    prompt = prompt.split(" ", 1)[1]
 
-            if not system_message:
-                for row in self.prompts:
-                    if row["Key Word"] == self.default_system_prompt:
-                        system_message = row["System Message"]
+        if len(prompt_array) == 1:
+            prompt = prompt_array[0]
 
-            answer = self.send_prompt(prompt, system_message)
+        logging.debug(
+            f"""
+        Prompt: {prompt}
+        Prompt keyword: {prompt_keyword}
+        System message: {system_message}
+        """
+        )
 
-            if answer:
-                answer = answer.lstrip("\n").lstrip("\n")
-                short_answer = self.ellipsis(answer, 30)
+        return prompt, prompt_keyword, system_message
 
-                self.add_item(
-                    title="Copy to clipboard",
-                    subtitle=f"Answer: {short_answer}",
-                    method=self.copy_answer,
-                    parameters=[answer],
-                )
-                self.add_item( 
-                    title="Open in text editor",
-                    subtitle=f"Answer: {short_answer}",
-                    method=self.open_in_editor,
-                    parameters=[answer],
-                )
-        else:
-            self.add_item(
-                title=f"Type your prompt and end with {self.prompt_stop}",
-                subtitle=f"Current model: {self.model}",
-            )
-        return
-    
     def ellipsis(self, string: str, length: int):
-        string = string.split('\n', 1)[0]
-        return string[:length-3] + '...' if len(string) > length else string
+        string = string.split("\n", 1)[0]
+        return string[: length - 3] + "..." if len(string) > length else string
 
     def copy_answer(self, answer: str) -> None:
         """
@@ -131,14 +209,21 @@ class ChatGPT(Flox):
         """
         pyperclip.copy(answer)
 
-    def open_in_editor(self, answer: str) -> None:
-        # Create a temporary file to store the content
-        temp_file = "temp_text.txt"
-        with open(temp_file, "w") as f:
-            f.write(answer)
+    def open_in_editor(self, filename: str | None, answer: str | None) -> None:
+        """
+        Open the answer in the default text editor. If no filename is given,
+        the conversation will be written to a new text file and opened.
+        """
+        if filename:
+            webbrowser.open(filename)
+            return
 
-        # Open the default editor and paste the content
-        webbrowser.open(temp_file)
+        if answer:
+            temp_file = "temp_text.txt"
+            with open(temp_file, "w") as f:
+                f.write(answer)
+            webbrowser.open(temp_file)
+            return
 
     def open_plugin_folder(self) -> None:
         webbrowser.open(os.getcwd())
